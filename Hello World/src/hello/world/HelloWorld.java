f4c20886746c0802316f75e19bddad4d2a565c81
SmartLemur <130591739+SmartLemur@users.noreply.github.com>
2024-11-15 00:13:11 +0800

    Chapter 1

---
+++ Hello World/src/hello/world/HelloWorld.java
@@ -0,0 +1,136 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello.world;
+
+/**
+ *
+ * @author Daniel
+ */
+//public class HelloWorld {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        System.out.print("Hello World!!");
+//
+//    //End of main
+// //End of HelloWorld Class
+//      
+//    }
+//}
+
+//public class HelloWorld {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        System.out.print("Yang Bener");
+//
+//    //End of main
+// //End of HelloWorld Class
+//      
+//    }
+//}
+
+//public class HelloWorld {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        System.out.print("My Name Is Harris");
+//        System.out.print("My Interest is Playing Games");
+//        System.out.print("My Fav Place is My Bedroom");
+//
+//    //End of main
+// //End of HelloWorld Class
+//      
+//    }
+//}
+
+//public class HelloWorld {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        System.out.println("My Dream Salary is 10K USD.\n My Dream Job is Software Engineer.\n My Fav Food is Chicken.");
+//        
+//    //End of main
+// //End of HelloWorld Class
+//      
+//    }
+//}
+
+//public class HelloWorld {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        System.out.println("===== THIS IS ARTITHMETICS OPERATORS =====\n"); 
+//        
+//       int A = 5;
+//       int B = 12;
+//    int sum = A + B;   
+//        System.out.println("           1. 5 + 12 = " +(A+B));    
+//    
+//       int G = 3;
+//       int F = 1;
+//
+//        System.out.println("           2. 3 - 1 = " +(G-F));    
+//        
+//       int K = 5;
+//       int O = 5;
+//    
+//        System.out.println("           3. 5 / 5 = " +(K/O)); 
+//        
+//       int J = 12;
+//       int M = 12;
+//      
+//        System.out.println("           4. 12 X 12 = " +(J*M));  
+//        
+//        System.out.println("=========================================="); 
+//        
+//    
+//       
+//        
+//    //End of main
+// //End of HelloWorld Class
+//      
+//    }
+//}
+
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(" ======================================"); 
+        System.out.println("(         WELCOME TO MY STORE          )"); 
+        System.out.println(" ======================================"); 
+        System.out.println("(         List of Beverages            )"); 
+        System.out.println("( 1. Chocolate Milk Godday  : RM 2.00  )"); 
+        System.out.println("( 2. Smoothie Iced Latte    : RM 4.00  )"); 
+        System.out.println("( 3. Pop                    : RM 1.50  )\n"); 
+        System.out.println("----------------------------------------"); 
+        System.out.println("Insert code of your desired drinks : 1  \n"); 
+        System.out.println("Insert quantity: 2  \n"); 
+        System.out.println("Chocolate Milk Godday : RM 2  \n"); 
+        System.out.println("Total Price : RM 4  "); 
+        
+//      
+        
+    //End of main
+ //End of HelloWorld Class
+      
+    }
+}
+
+
+    
